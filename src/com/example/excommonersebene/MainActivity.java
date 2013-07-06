3d94be9b40d5acd3d3bb35712f9fea7830d54335
Nayar Joolfoo <njoolfoo@gmail.com>
2013-07-06 21:28:35 +0400

    Deleted Extra files and added pointers

--- src/com/example/excommonersebene/MainActivity.java
+++ src/com/example/excommonersebene/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.excommonersebene;
 
+import java.util.ArrayList;
+
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,7 +11,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
+
+import com.example.excommonersebenelib.*;
 
 public class MainActivity extends Activity {
 
@@ -20,7 +25,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		BusStops bustops = new BusStops();
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		
+		/*BusStops bustops = new BusStops();
 		bustops.add(1,20.1625, 58.2903);
 		bustops.add(2,5, 5);
 		bustops.add(3,-26.4545, 7.125);
@@ -31,9 +39,32 @@ public class MainActivity extends Activity {
 		while(current != null){
 			map.addMarker(new MarkerOptions().position(new LatLng(current.x, current.y)).title("find me here"));
 			current = current.next;
+		}*/
+		
+		BusStop b1,b2,b3,b4,b5,b6,b7,b8;
+		Route r1,r2,r3;
+		ArrayList<Route> routes = new ArrayList<Route>();
+		ArrayList<BusStop> bustops = new ArrayList<BusStop>();
+		bustops.add(b1 = new BusStop(1, -20.23215, 57.498530));
+		bustops.add(b2 = new BusStop(2, -20.232498, 57.497736));
+		bustops.add(b3 = new BusStop(3, -20.242982, 57.491942));
+		bustops.add(b4 = new BusStop(4, -20.242720, 57.491830));
+		bustops.add(b5 = new BusStop(5,-20.243214, 57.490811));
+
+		bustops.add(b6 = new BusStop(6,-20.231667, 57.501765));
+
+		bustops.add(b7 = new BusStop(7,-20.229226, 57.503385));
+
+		bustops.add(b8 = new BusStop(8,-20.243586, 57.488906));
+		r1 = new Route(1);
+		r2 = new Route(2);
+		//r1.addBusStop(b1);
+		
+		for(int i = 0;i<bustops.size();i++){
+			map.addMarker(new MarkerOptions().position(new LatLng(bustops.get(i).x, bustops.get(i).y)).title("Bus Stop here"));
 		}
-		
-		
+		//MeraDatabase mDbHelper = new MeraDatabase(getBaseContext());
+		//SQLiteDatabase db = MeraDatabase.;
 		
 		//CameraUpdate update = CameraUpdateFactory.newLatLngZoom(location_maritiusLatLng, 15);
 		//map.animateCamera(update);
--- src/com/example/excommonersebene/Route.java
+++
@@ -1,6 +0,0 @@
-package com.example.excommonersebene;
-
-public class Route {
-	int no;
-	BusStop bss[];
-}
